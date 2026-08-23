1775c892e7cba1e0700e193e7ccba9ee8e2cb3cc
iftix0r <iftixortoychiyev1@gmail.com>
2026-08-23 20:03:16 +0500

    feat: customize admin panel branding and refine navbar layout and navigation items

--- aslmarket/urls.py
+++ aslmarket/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "AslFood Boshqaruv Paneli"
+admin.site.site_title = "AslFood Admin"
+admin.site.index_title = "Tizim Boshqaruvi"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
